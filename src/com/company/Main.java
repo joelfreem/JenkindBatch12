c07a033fc73948ff3252262bc214aeb2cbb8a9cb
joelfreem <joelfreem@gmail.com>
2022-06-21 21:43:17 -0400

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	for(int i=0;i<5;i++){
         System.out.println("Hello World");
+        System.out.println("Hello I am tired");
     }
     }
 }
